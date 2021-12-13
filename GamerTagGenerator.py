660fa307cc56f3942c171e7715a734301a72ec64
Oscar Orozco <62218092+Osormo50@users.noreply.github.com>
2021-12-12 22:45:46 -0800

    Add files via upload

---
+++ GamerTagGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Gamer Tag Generator")
+color = input("Insert your favorite Color: \n").capitalize()
+fruit = input("Insert your favorite fruit/vegetable: \n").capitalize()
+print(f"your gamer tag {color}{fruit}!")
